80da1253eb5451ec241f42a4a3e6db426a9fbf7d
Shane <vandesm14@gmail.com>
2026-04-15 18:48:33 -0400

    wip: petgraph stuff

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,93 @@
-use std::{any::Any, error::Error};
+use std::{error::Error, fmt};
 
-use crossbeam_channel::{Receiver, Sender};
-use oh_no::*;
-use petgraph::prelude::*;
+use petgraph::graph::{NodeIndex, UnGraph};
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+#[derive(Debug, Default)]
+struct MyPC;
+
+impl Computer for MyPC {
+  fn update(&mut self, incoming: &[Message], outgoing: &mut Vec<Message>) {
+    println!("messages: {incoming:#?}");
+  }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
   let mut world = World::default();
 
-  let computer_a = world.add_computer(Box::new(MyPC::new())).unwrap();
-  let computer_b = world.add_computer(Box::new(MyPC::new())).unwrap();
-  let computer_c = world.add_computer(Box::new(MyPC::new())).unwrap();
+  let computer_a = world.add_node(Node::new(Box::new(MyPC), Vec::new()));
+  let computer_b = world.add_node(Node::new(Box::new(MyPC), Vec::new()));
+  let computer_c = world.add_node(Node::new(Box::new(MyPC), Vec::new()));
 
   world.connect(computer_a, computer_b);
   world.connect(computer_b, computer_c);
 
   world.update();
-  world.update();
+
+  println!("{world:#?}");
 
   Ok(())
 }
 
-#[derive(Debug, Clone)]
-struct MyPC {
-  sender: Sender<Message>,
-  receiver: Receiver<Message>,
+#[derive(Debug)]
+enum Connection {
+  Wired,
+  Wireless { distance: f32 },
 }
 
-impl MyPC {
-  fn new() -> Self {
-    let (sender, receiver) = crossbeam_channel::unbounded();
+#[derive(Debug)]
+struct Message;
 
-    Self { sender, receiver }
+trait Interface: fmt::Debug {
+  fn handle(&mut self, message: Message) -> Option<Message>;
+}
+
+trait Computer: fmt::Debug {
+  fn update(&mut self, incoming: &[Message], outgoing: &mut Vec<Message>);
+}
+
+#[derive(Debug)]
+struct Node {
+  computer: Box<dyn Computer>,
+  interfaces: Vec<usize>,
+}
+
+impl Node {
+  #[inline]
+  pub const fn new(
+    computer: Box<dyn Computer>,
+    interfaces: Vec<usize>,
+  ) -> Self {
+    Self {
+      computer,
+      interfaces,
+    }
   }
 }
 
-impl Computer for MyPC {
-  fn setup(&mut self) -> Result<(), Box<dyn Error>> {
-    Ok(())
+#[derive(Debug, Default)]
+struct World {
+  interfaces: Vec<Box<dyn Interface>>,
+  computers: UnGraph<Node, ()>,
+  messages: Vec<Message>,
+  next_messages: Vec<Message>,
+}
+
+impl World {
+  pub fn add_node(&mut self, node: Node) -> NodeIndex {
+    self.computers.add_node(node)
   }
 
-  fn update(
-    &mut self,
-    edges: Vec<EdgeIndex>,
-    id: ComputerId,
-  ) -> Result<Vec<Message>, Box<dyn Error>> {
-    let incoming = self.receiver.try_iter().collect::<Vec<_>>();
-    println!("{:?}: {:?}", id, incoming);
-
-    // Ok(vec![Message { data: vec![] }])
-    Ok(
-      edges
-        .into_iter()
-        .map(|edge| Message {
-          data: vec![],
-          edge,
-          from: id,
-        })
-        .collect(),
-    )
+  pub fn connect(&mut self, a: NodeIndex, b: NodeIndex) {
+    self.computers.add_edge(a, b, ());
   }
 
-  fn incoming(&self) -> &Sender<Message> {
-    &self.sender
-  }
+  pub fn update(&mut self) {
+    for node in self.computers.node_weights_mut() {
+      node
+        .computer
+        .update(&self.messages, &mut self.next_messages);
+    }
 
-  fn as_any(&self) -> &dyn Any {
-    self
+    self.messages = core::mem::take(&mut self.next_messages);
   }
 }
